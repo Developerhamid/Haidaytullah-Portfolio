af053a267f339188a125ada963008caa68a5c137
Developerhamid <iamdeveloperhamid@gmail.com>
2022-07-16 10:48:28 +0530

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,14 +1,12 @@
-let burgur = document.querySelector(".burgur");
-let navbar = document.querySelector(".navbar");
-let logo = document.querySelector(".logo");
-let navlist = document.querySelector(".nav-list");
-let hire = document.querySelector(".hire");
+let Nav = document.querySelector('nav');
+let MenuBar = document.querySelector('#menu_bar');
+let Navigation = document.querySelector('.navigation');
+let Hiring = document.querySelector('.hiring');
 
-burgur.addEventListener("click", () => {
-  setTimeout(() => {
-    navbar.classList.toggle("nav-height");
-    logo.classList.toggle("nav-hidden");
-    navlist.classList.toggle("nav-hidden");
-    hire.classList.toggle("nav-hidden");
-  }, 200);
-});
+MenuBar.addEventListener('click', () =>{
+    setTimeout(() => {
+        Nav.classList.toggle('nav_height')
+        Navigation.classList.toggle('hidden');
+        Hiring.classList.toggle('hidden');  
+    }, 100);
+})
